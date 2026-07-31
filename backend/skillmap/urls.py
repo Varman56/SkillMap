9be296ef9687fd1b49bb8a73b8b012e192ceeee1
lamborghini_Diablo <168207032+levanchio@users.noreply.github.com>
2026-07-31 11:42:11 +0500

    Доделанные страницы авторизаци и профиля

--- backend/skillmap/urls.py
+++ backend/skillmap/urls.py
@@ -24,7 +24,49 @@ urlpatterns = [
     
     # Остальные страницы пока оставляем как есть, 
     # позже для них тоже нужно будет написать свои views
-    path("profile/edit/", TemplateView.as_view(template_name="edit_profile.html"), name="edit_profile"),
+    path("profile/edit/", TemplateView.as_view(template_name="edit_profile.html", extra_context ={
+        'skills': [
+            {'level_class': 'expert', 'level_name': 'Эксперт', 'name': 'Работа с диаграммами (Miro)'},
+            {'level_class': 'advanced', 'level_name': 'Продвинутый', 'name': 'Работа с Figma'},
+            {'level_class': 'advanced', 'level_name': 'Продвинутый', 'name': 'Работа с Webflow'},
+            {'level_class': 'novice', 'level_name': 'Новичок', 'name': 'Базы данных (Redis)'},
+            {'level_class': 'novice', 'level_name': 'Новичок', 'name': 'Базы данных (MySQL)'},
+            {'level_class': 'experienced', 'level_name': 'Опытный', 'name': 'Языки программирования (Python)'},
+            {'level_class': 'novice', 'level_name': 'Новичок', 'name': 'Языки программирования (Golang)'},
+        ],
+        'projects': [
+            {
+                'id': 1, 
+                'icon': 'Component-1.svg', 
+                'title': 'Платформа деплоя', 
+                'description': 'Разработка макетов платформы для автоматического деплоя сервисов'
+            },
+            {
+                'id': 2, 
+                'icon': 'Component-2.svg', 
+                'title': 'Веб-сайт учета мероприятий', 
+                'description': 'Разработка пользовательских сценариев и макетов веб-сайта'
+            },
+            {
+                'id': 3, 
+                'icon': 'Component-3.svg', 
+                'title': 'Платформа трассировки инженерных сетей', 
+                'description': 'Разработка макетов платформы; интеграция базы данных'
+            },
+            {
+                'id': 4, 
+                'icon': 'Component-4.svg', 
+                'title': 'Платформа для компетенций сотрудников', 
+                'description': 'Разработка макетов веб-сайта для главной страницы; разработка ui-kit'
+            },
+            {
+                'id': 5, 
+                'icon': 'Component-5.svg', 
+                'title': 'Визуальная обучающая игра', 
+                'description': 'Отрисовка спрайтов персонажей, разработка динамиечских макетов главного меню'
+            },
+        ]
+    }), name="edit_profile"),
     path("profile/", TemplateView.as_view(template_name="profile.html"), name="profile"),
     path("matrix/", TemplateView.as_view(template_name="matrix.html"), name="matrix"),
     path("hr/", TemplateView.as_view(template_name="hr.html"), name="hr"),
